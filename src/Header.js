c2bd6de8680b6586a3e9d6624c48e0c9ce8a0d27
Louie Camacho <louiecamacho@Louies-MacBook-Pro-4.local>
2020-10-02 19:55:13 -0500

    map through social media links

--- src/Header.js
+++ src/Header.js
@@ -3,26 +3,43 @@ import React from 'react';
 class Header extends React.Component {
 	render() {
 		const { name, linkText, linkSrc } = this.props; //destructuring props (es6) same as this.props.[prop]
+
+		// build social links and icons as objects
+		const socialLinks = [
+			{
+				link: 'https://github.com/buttonbag',
+				icon: 'fab fa-github',
+			},
+			{
+				link: 'https://codepen.io/buttonbag',
+				icon: 'fab fa-codepen',
+			},
+			{
+				link: 'https://www.linkedin.com/in/louiecamacho/',
+				icon: 'fab fa-linkedin',
+			},
+			{
+				link: 'https://twitter.com/iambuttonbag',
+				icon: 'fab fa-twitter',
+			},
+		];
+
+		// map through each social media links and icons
+		const socLinksLoop = socialLinks.map((soc) => {
+			return (
+				<a href={soc.link} key={soc.link}>
+					<li className={soc.icon}></li>
+				</a>
+			);
+		});
+
 		return (
 			<div className="header">
 				<h1 className="name">{name}</h1>
 
 				<div className="contact">
 					<a href={linkSrc}>{linkText}</a>
-					<span className="social">
-						<a href="https://github.com/buttonbag">
-							<i className="fab fa-github"></i>
-						</a>
-						<a href="https://codepen.io/buttonbag">
-							<i className="fab fa-codepen"></i>
-						</a>
-						<a href="https://www.linkedin.com/in/louiecamacho/">
-							<i className="fab fa-linkedin"></i>
-						</a>
-						<a href="https://twitter.com/iambuttonbag">
-							<i className="fab fa-twitter"></i>
-						</a>
-					</span>
+					<span className="social">{socLinksLoop}</span>
 					<a href="mailto:louiecamacho@gmail.com">louieCamacho@gmail.com</a>
 					<span>773.952.0534</span>
 				</div>
