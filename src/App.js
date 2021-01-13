773b897b3cbcdd55095259ef4347122bcd54eb2b
it-loaner <louiecamacho@gmail.com>
2021-01-13 11:44:25 -0600

    restart

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
-import React, { Component } from 'react';
-import Header from './Header';
-import Profile from './Profile';
-import Skills from './Skills';
-import Work from './Work';
-import Education from './Education';
+import './App.css';
+import { Header } from './Header';
+import { Body } from './Body';
+import { Footer } from './Footer';
 
-class App extends Component {
-	render() {
-		return (
-			<div className="App container">
-				<Header
-					name="Louie Camacho"
-					linkSrc="https://www.iambuttonbag.com"
-					linkText="IamButtonbag.com"
-				/>
-
-				<div className="content">
-					<Profile />
-
-					<Skills />
-
-					<Work />
-
-					<Education />
-				</div>
-			</div>
-		);
-	}
+function App() {
+	return (
+		<main id="main">
+			<Header name="Louie" />
+			<Body />
+			<Footer />
+		</main>
+	);
 }
 
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -1,51 +1,18 @@
 import React from 'react';
 
-class Header extends React.Component {
+export class Header extends React.Component {
 	render() {
-		const { name, linkText, linkSrc } = this.props; //destructuring props (es6) same as this.props.[prop]
-
-		// build social links and icons as objects
-		const socialLinks = [
-			{
-				link: 'https://github.com/buttonbag',
-				icon: 'fab fa-github',
-			},
-			{
-				link: 'https://codepen.io/buttonbag',
-				icon: 'fab fa-codepen',
-			},
-			{
-				link: 'https://www.linkedin.com/in/louiecamacho/',
-				icon: 'fab fa-linkedin',
-			},
-			{
-				link: 'https://twitter.com/iambuttonbag',
-				icon: 'fab fa-twitter',
-			},
-		];
-
-		// map through each social media links and icons
-		const socLinksLoop = socialLinks.map((soc) => {
-			return (
-				<a href={soc.link} key={soc.link}>
-					<li className={soc.icon}></li>
-				</a>
-			);
-		});
-
 		return (
-			<div className="header">
-				<h1 className="name">{name}</h1>
-
-				<div className="contact">
-					<a href={linkSrc}>{linkText}</a>
-					<span className="social">{socLinksLoop}</span>
-					<a href="mailto:louiecamacho@gmail.com">louieCamacho@gmail.com</a>
-					<span>773.952.0534</span>
-				</div>
-			</div>
+			<header>
+				<h1>Sup, {this.props.name}!</h1>
+				<h2>Sup, {this.props.name}!</h2>
+				<h3>Sup, {this.props.name}!</h3>
+				<h4>Sup, {this.props.name}!</h4>
+				<h5>Sup, {this.props.name}!</h5>
+				<h6>Sup, {this.props.name}!</h6>
+			</header>
 		);
 	}
 }
 
-export default Header;
+Header.defaultProps = { name: 'Default Name' };
