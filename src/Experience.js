60a59f49f91c4b034d76d1c811ce536b28f8c941
Louie Camacho <louiecamacho@gmail.com>
2021-01-25 15:24:18 -0600

    added footer css and experience loop

--- src/Experience.js
+++ src/Experience.js
@@ -2,23 +2,49 @@ import React from 'react';
 import './Experience.css';
 // import ReactDOM from 'react-dom';
 
+const entry = [
+	{
+		date: 'date 1',
+		position: 'position 1',
+		description:
+			'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum sequi placeat ex dignissimos fuga deleniti commodi tenetur eos minima, dolor, quis error facilis nobis amet asperiores pariatur molestiae ipsum quo?',
+	},
+	{
+		date: 'date 2',
+		position: 'position 2',
+		description:
+			'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum sequi placeat ex dignissimos fuga deleniti commodi tenetur eos minima, dolor, quis error facilis nobis amet asperiores pariatur molestiae ipsum quo?',
+	},
+	{
+		date: 'date 3',
+		position: 'position 3',
+		description:
+			'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum sequi placeat ex dignissimos fuga deleniti commodi tenetur eos minima, dolor, quis error facilis nobis amet asperiores pariatur molestiae ipsum quo?',
+	},
+];
+
 export class Experience extends React.Component {
+	entries = entry.map((work, index) => {
+		return (
+			<section key={index}>
+				<div className="entry" key={index}>
+					<div className="date" key={work.date.index}>
+						{work.date}
+					</div>
+					<div className="position" key={work.position.index}>
+						{work.position}
+					</div>
+					<p key={work.description.index}>{work.description}</p>
+				</div>
+			</section>
+		);
+	});
+
 	render() {
 		return (
 			<div id="experience">
 				<h2>Experience</h2>
-				<section>
-					<div className="entry">
-						<div className="date">date</div>
-						<div className="position">position</div>
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque
-							eum deleniti doloremque facere accusantium provident aspernatur
-							non, a fuga repudiandae debitis veniam perferendis eius libero
-							iusto rem porro perspiciatis minima!
-						</p>
-					</div>
-				</section>
+				{this.entries}
 			</div>
 		);
 	}
--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import './footer.css';
 
 export class Footer extends React.Component {
 	render() {
 		return (
 			<footer>
-				<p>this is a footer</p>
+				<section>
+					<h2 className="title">Education</h2>
+					<ul>
+						<li>date</li>
+						<li>degree</li>
+						<li>school</li>
+					</ul>
+				</section>
+				<section>
+					<h2 className="title">interests</h2>
+					<ul>
+						<li>item</li>
+						<li>item</li>
+						<li>item</li>
+						<li>item</li>
+					</ul>
+				</section>
+				<section>
+					<h2 className="title">contact</h2>
+					<ul>
+						<li>email</li>
+						<li>phone</li>
+					</ul>
+					<ul>
+						<li>links</li>
+						<li>links</li>
+						<li>links</li>
+					</ul>
+				</section>
 			</footer>
 		);
 	}
